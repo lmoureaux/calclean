6196579c3f6d95ddf36985f07f10ecab51a6948e
Louis Moureaux <m_louis30@yahoo.com>
2017-03-03 16:55:54 +0100

    Add tool to print position of hot cells

--- tools/hbanalyzer.lua
+++ tools/hbanalyzer.lua
@@ -1,13 +1,14 @@
 -- Author: Louis Moureaux
 -- Year:   2017
 
+local math = require('math')
 local os = require('os')
 
 function printusage()
   print('Usage: ' .. arg[0] .. ' <datafile> <action> <args>...')
   print('Where:')
   print('  <datafile> is a file containing the output of hbanalyzer')
-  print('  <action> is one of the following: slice')
+  print('  <action> is one of the following: slice, energy, hotcells')
   print('  <args> depend on the specified action')
 end
 
@@ -53,7 +54,24 @@ elseif arg[2] == 'energy' then
     end
     print(16, 1.4025, 0, 0)
   else
-    print('Error: missing parameter for action \'nslice\'')
+    print('Error: missing parameter for action \'energy\'')
+    os.exit(1)
+  end
+elseif arg[2] == 'hotcells' then
+  if #arg >= 3 then
+    ok, spec = pcall(loadfile(arg[3]))
+    if not ok then
+      print('Error: cannot load \'' .. arg[3] .. '\'')
+      os.exit(1)
+    end
+    print('ieta', 'eta', 'iphi', 'phi')
+    for ieta, numhot in pairs(spec) do
+      for _, iphi in ipairs(data[numhot][ieta]) do
+        print(ieta, (ieta + 0.5) * 0.085, iphi, (iphi + 0.5) * math.pi / 36)
+      end
+    end
+  else
+    print('Error: missing parameter for action \'hotcells\'')
     os.exit(1)
   end
 else
